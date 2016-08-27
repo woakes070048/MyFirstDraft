3b32b7877faf9673036daab65c76478f0fdafeca
Devin <devinwiebelt@gmail.com>
2016-08-27 13:28:18 -0400

    setting up passport to establish user authentication

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ var logger = require('morgan');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
-var connection = require('./config/connection.js');
 var path = require('path');
 var passport = require('passport');
 var localStrategy = require('passport-local');
 var twitterStrategy = require('passport-twitter');
 var facebookStrategy = require('passport-facebook');
 var googleStrategy = require('passport-google');
-var session  = require('express-session');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var flash = require('connect-flash');
 var app = express();
@@ -37,13 +36,17 @@ app.use(session({
 	saveUninitialized: true,
  } ));
 
-//flash is used to show a message on an incorrect login
-app.use(flash());
+//===============PASSPORT===============
+
+//This section will contain our work with Passport
+
 
 // use passport authentication middleware 
 app.use(passport.initialize());
 app.use(passport.session());
 
+//flash is used to show a message on an incorrect login
+app.use(flash());
 
 //===============EXPRESS================
 // Configure Express
@@ -80,6 +83,40 @@ var hbs = exphbs.create({
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+
+//===============ROUTES=================
+//displays our homepage
+app.get('/', function(req, res){
+  res.render('home', {user: req.user});
+});
+
+//displays our signup page
+app.get('/signin', function(req, res){
+  res.render('signin');
+});
+
+//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/local-reg', passport.authenticate('local-signup', {
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/login', passport.authenticate('local-signin', {
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//logs user out of site, deleting them from the session, and returns to homepage
+app.get('/logout', function(req, res){
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
+  req.logout();
+  res.redirect('/');
+  req.session.notice = "You have successfully been logged out " + name + "!";
+});
 
 //===========PORT=====================
 
