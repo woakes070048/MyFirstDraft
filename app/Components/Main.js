292a09fd53659481cbd9d19df77bbfc745fbcf93
Devin <devinwiebelt@gmail.com>
2016-09-02 15:51:54 -0400

    handlebars files connected, can begin style and design of the site

---
+++ app/Components/Main.js
@@ -0,0 +1,12 @@
+// Include React 
+var React = require('react');
+
+// Here we include all of the sub-components
+var Form = require('./Children/Form');
+var Results = require('./Children/Results');
+
+// Helper Function
+var helpers = require('./utils/helpers.js');
+
+// This is the main component. 
+var Main = React.createClass({
